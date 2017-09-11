4bf9a98ed0e9eabfef5ebfda01a03210d98c5642
xiaoyu <530881987@qq.com>
2017-09-11 19:28:33 +0800

    form

---
+++ php_form_01.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaoyu
+ * Date: 2017/9/6
+ * Time: 22:32
+ */
+
+echo 'name is '.$_POST['name'];echo '<br>';
+echo 'email is '.$_POST['email'];echo '<br>';
